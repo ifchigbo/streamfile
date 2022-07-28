6588914c2693eea9ab94af5cf083d092339eaa27
ifchigbo <ifchigbo@gmail.com>
2022-07-28 16:18:32 +0100

    commit plots

--- streamtest.py
+++ streamtest.py
@@ -4,21 +4,23 @@ from sqlalchemy import column
 import streamlit as st
 from PIL import Image
 from streamlit.errors import Error
-from covid import getDataSource,getCountryStats
+from covid import getDataSource,getCountryStats, getGroupedData
 import streamlit.components as comp 
+import seaborn as sb
+from matplotlib import pyplot as plt
 
 st.set_page_config(
      page_title="Covid-19 Data Processing",
      page_icon="🧊",
      layout="wide",
      initial_sidebar_state="expanded",
-     menu_items={
-        '''''
-         'Get Help': '',
-         'Report a bug':,
-         'About': 
-         '''
-     }
+     #menu_items={
+     #   '''''
+     #    'Get Help': '',
+     #    'Report a bug':,
+     #    'About': 
+     #    '''
+     #}
 )
 def title_Section():
     try:
@@ -79,17 +81,57 @@ def covidStatsCountry():
 
     if  country:
         try:
-
             mycountry = getCountryStats(country)
             st.write(mycountry)
         except Error  as error:
             print(error)
 
+# Get Regional Deathrates for the past 7 days
+def groupedDatabyRegion():
+    try:
+        region = getGroupedData()
+        return region 
+    except Error as  error:
+        print(error)
+
+#Get plot from the regional data generated from motarlity  rates for the past 7 days
+def getRegionalPlotData():
+    plotvar =groupedDatabyRegion()
+    plot = st.selectbox(
+        "Select a Plot Style to Display",
+        ['Bar Plot', 'Line Plot']
+    )
+    if plot == 'Bar Plot':
+        try:
+            fig = plt.figure(figsize=(25,15))
+            sb.barplot(data=plotvar,y='Deaths - newly reported in last 7 days', x='region')
+            st.pyplot(fig)
+        except Error as error:
+            print(error)
+    elif plot == 'Line Plot':
+        try:
+            fig = plt.figure(figsize=(25,15))
+            #sb.relplot(x='region', y='Deaths - newly reported in last 7 days',kind='line', data=plotvar)
+            sb.lineplot(x='region', y='Deaths - newly reported in last 7 days', data=plotvar)
+            st.pyplot(fig)
+        except Error as error:
+            print(error)
+            
+
+    
+
+
     #The main method that runs all files
 def allMethods():
+    sb.set_theme()
     title_Section()
     displayInitialAnalysis()
     covidStatsCountry()
+    print(st.subheader('Regional Death Rate in  7 Days'))
+    st.write(
+        groupedDatabyRegion()   
+    )
+    getRegionalPlotData()
 
 if __name__ == '__main__':
     allMethods()
